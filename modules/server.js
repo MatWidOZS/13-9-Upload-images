478177ad322dcb1913e25fa2659d853fbc6db484
Mateusz Widerski <mat.widerski@gmail.com>
2018-04-11 18:48:39 +0200

    server.js - Add switch for distinguish between queries

--- modules/server.js
+++ modules/server.js
@@ -1,12 +1,26 @@
 var http = require('http');
 var colors = require('colors');
 
+var handlers = require('./handlers'); //my module
+
 function start() {
-	function onRequest(requers, response) {
-		console.log('Odebrano zapytanie.');
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write('Pierwsze koty za płoty');
-		response.end();
+	function onRequest(request, response) {
+		console.log('Odebrano zapytanie.'.green);
+		console.log('Zapytanie ' + request.url + ' odebrane.');
+
+		response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+
+		switch (request.url) { //switch for distinguish between queries
+			case '/':
+			case '/start':
+				handlers.welcome(request, response);
+				break;
+			case '/upload':
+				handlers.upload(request, response);
+				break;
+			default:
+				handlers.error(request, response);
+		}
 	}
 
 	http.createServer(onRequest).listen(9000);
