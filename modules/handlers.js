3a61e721b0dce9738fa90b93aece4fc8299e8c19
Mateusz Widerski <mat.widerski@gmail.com>
2018-04-14 15:07:28 +0200

    Add function for upload and show uploaded image

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,14 +1,21 @@
 var fs = require('fs');
+var formidable = require('formidable');
 
 exports.upload = function(request, response) {
 	console.log('Rozpoczynam obsługę żądania upload...');
-	response.write('Rozpoczynam upload!');
-	response.end();
+	var form = new formidable.IncomingForm();
+	form.parse(request, function(error, fields, files) {
+		fs.renameSync(files.upload.path, "test.png");
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('received image:<br>');
+		response.write('<img src="/show">');
+		response.end();		
+	});
 }
 
 exports.welcome = function(request, response) {
 	console.log('Rozpoczynam obsługę żądania welcome...');
-	fs.readFile('../templates/start.html', function(err, html) {
+	fs.readFile('templates/start.html', function(err, html) {
 		response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 		response.write(html);
 		response.end();
@@ -19,4 +26,12 @@ exports.error = function(request, response) {
 	console.log('Nie wiem co robić...');
 	response.write('404 :(');
 	response.end();
+}
+
+exports.show = function(request, response) {
+	fs.readFile('test.png', 'binary', function(error, file) {
+		response.writeHead(200, {'Content-Type': 'image/png'});
+		response.write(file, 'binary');
+		response.end();
+	});
 }
