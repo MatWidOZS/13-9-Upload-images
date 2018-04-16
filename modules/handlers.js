8c73b83d38d505f4c0e0cd5c2bdbd31427539bca
Mateusz Widerski <mat.widerski@gmail.com>
2018-04-16 11:26:11 +0200

    Fix show image size

--- modules/handlers.js
+++ modules/handlers.js
@@ -7,8 +7,8 @@ exports.upload = function(request, response) {
 	form.parse(request, function(error, fields, files) {
 		fs.renameSync(files.upload.path, "test.png");
 		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('received image:<br>');
-		response.write('<img src="/show">');
+		response.write('<h1>Received image:</h1><br>');
+		response.write('<img src="/show" width="800">');
 		response.end();		
 	});
 }
